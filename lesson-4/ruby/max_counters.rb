0c4a61aa7a0fd032144043c03f80854dd11a21c7
daniellizik-sc <daniel.lizik@salecycle.com>
2016-12-20 16:05:40 -0500

    counters ruby

--- lesson-4/ruby/max_counters.rb
+++ lesson-4/ruby/max_counters.rb
@@ -1,3 +1,13 @@
-def solution(a)
-  
+def solution(n, a)
+  counters = (0..a.length - 1).map { 0 }
+  a.each do |k, i|
+    if k == (n + 1)
+      counters = a.map { |x, j|  } # reduce largest and set
+    else
+      counters[i] += 1
+    end
+  end
+  counters
 end
+
+print solution(5, [3, 4, 4, 2, 4])
--- lesson-4/ruby/max_counters.spec.rb
+++ lesson-4/ruby/max_counters.spec.rb
@@ -4,10 +4,7 @@ require "test/unit"
 class Spec < Test::Unit::TestCase
 
   def test_solution
-    assert solution([1, 2, 3, 4]) == 1, 'should return 1'
-    assert solution([1, 3, 3]) == 0, 'should return 0'
-    assert solution([1, 3, 2, 4]) == 1, 'should return 1'
-    assert solution([3, 1, 2, 6, 7, 5, 4]) == 1, 'should return 1'
+    assert_equal solution(6, [3, 4, 4, 6, 1, 4, 4]), [3, 2, 2, 4, 2]
   end
 
 end
