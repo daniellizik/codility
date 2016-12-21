536df658ccbd23c810328bb05bbef1cc8bb7aed8
daniellizik <daniellizik@gmail.com>
2016-12-21 10:14:48 -0500

    js naming

--- lesson-4/js/maxCounters.js
+++ lesson-4/js/maxCounters.js
@@ -1,23 +1,23 @@
 module.exports = function solution(n, a) {
 
-  let max = 0;
-  let last = 0;
   let counters = new Array(n).fill(0);
+  let incrementMax = 0;
+  let incrementLast = 0;
 
   a.forEach(num => {
     let index = num - 1;
     if (num > n)
-      max = last;
+      incrementMax = incrementLast;
     else {
-      if (max > counters[index])
-        counters[index] = max + 1;
+      if (incrementMax > counters[index])
+        counters[index] = incrementMax + 1;
       else
         counters[index] += 1;
-      if (counters[index] > last)
-        last = counters[index];
+      if (counters[index] > incrementLast)
+        incrementLast = counters[index];
     }
   });
 
-  return counters.map(num => num < max ? max : num);
+  return counters.map(num => num < incrementMax ? incrementMax : num);
 
 };
