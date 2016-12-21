a27131a7757236ea63f803000e60694f1849382d
daniellizik-sc <daniel.lizik@salecycle.com>
2016-12-21 10:11:41 -0500

    max counters js, ruby

--- lesson-4/js/maxCounters.js
+++ lesson-4/js/maxCounters.js
@@ -1,3 +1,23 @@
-export default function(n, a) {
-  
-}
+module.exports = function solution(n, a) {
+
+  let max = 0;
+  let last = 0;
+  let counters = new Array(n).fill(0);
+
+  a.forEach(num => {
+    let index = num - 1;
+    if (num > n)
+      max = last;
+    else {
+      if (max > counters[index])
+        counters[index] = max + 1;
+      else
+        counters[index] += 1;
+      if (counters[index] > last)
+        last = counters[index];
+    }
+  });
+
+  return counters.map(num => num < max ? max : num);
+
+};
--- lesson-4/ruby/max_counters.rb
+++ lesson-4/ruby/max_counters.rb
@@ -1,16 +1,21 @@
 def solution(n, a)
-  counters = (0..n - 1).map { 0 }
-  largest = 0
-  increment = 1
-  a.each_index do |i|
-    if a[i] == (n + 1)
-      counters = (0..n - 1).map { largest }
-    elsif (1 <= a[i]) && (a[i] <= n)
-      counters[a[i] - 1] += 1
-      largest = counters[a[i] - 1]
+  counters = Array.new(n, 0)
+  increment_previous = 0
+  increment_max = 0
+  a.each do |i|
+    pos = i - 1;
+    if i > n
+      increment_max = increment_previous
+    else
+      if increment_max > counters[pos]
+        counters[pos] = increment_max + 1
+      else
+        counters[pos] += 1
+      end
+      if counters[pos] > increment_previous
+        increment_previous = counters[pos]
+      end
     end
   end
-  counters
+  counters.map { |x| x < increment_max ? increment_max : x }
 end
-
-solution(5, [3, 4, 4, 6, 1, 4, 4])
